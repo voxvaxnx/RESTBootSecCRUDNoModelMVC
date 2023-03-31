6bc8d92f711a33554793960911dc68e2f8c29e64
voxvaxnx <voxvaxnx@yandex.ru>
2023-03-31 15:15:09 +0500

    Исправил Замечания

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImp.java
@@ -0,0 +1,28 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import java.util.List;
+
+@Service
+public class RoleServiceImp implements RoleService{
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImp(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<Role> getRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Role getRoleById(Long id) {
+        return roleRepository.getById(id);
+    }
+}
